83d06bb66013f178ee2370eaffefe7f15d96549e
CARRERO Luca <carrero.luca@gitlab.fermimn.edu.it>
2020-09-05 10:51:46 +0200

    inserito alcune idee

--- lib/KeyController/KeyController.h
+++ lib/KeyController/KeyController.h
@@ -9,14 +9,14 @@ class KeyController{
         int up;
         int down;
         int left;
-        int right;
+        int right; // puntatori a funzione?
         int a;
         int b;
     public:
-        bool loadKeyMap();
-        bool isUpPressed();
+        bool begin();
+        bool isUpPressed(); // usarle con un puntatore a funzione come call back?  così da configurarle nel setup e una fuzione unica nel loop che controlla i pulsanti.
         bool isDownPressed();
-        bool idLeftPressed();
+        bool isLeftPressed();
         bool isRightPressed();
         bool isAPressed();
         bool isBPressed();
