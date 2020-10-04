fcab7906cf675ab78cc290f5a3757bb2c7ab85d6
CarreroLuca <oooiiii891@gmail.com>
2020-10-04 09:44:42 +0200

    aggiunto commenti

--- lib/GameObject/GameObject.h
+++ lib/GameObject/GameObject.h
@@ -3,24 +3,35 @@
 
 #include "Arduino.h"
 
-#ifndef _U8G2LIB_HH
-#include <U8g2lib.h>
-#endif
 
+#include <U8g2lib.h>
+
+
+/*
+    Classe di base su cui basare gli oggetti di gioco.
+    Estendendo questa classe si ha la possibiltà di gestire di usufruire di alcuni metodi relativi alla gestione delle cordinate.
+    Per la scelta di cosa disegnare a schermo si totalmente totalmente liberisi può gestire tramite metodi esterno o fancendo l'override del metodo disegna.
+*/
 class GameObject {
 private:
-    /* data */
+    /* attributi relativi alle cordinate e alle dimensioni dell'oggetto */
     int x;
     int y;
     int h;
     int b;
 public:
     GameObject(int,int,int,int);
-    virtual void disegna(U8G2 &t);
+    #ifndef _U8G2LIB_HH
+    // metodo da override per castomizzare casa disegnare sul display
+     virtual void disegna(U8G2 &t);
+    #endif
+    //getter degli attributi della classe
     int getX();
     int getY();
     int getH();
     int getB();
+
+    //setter degli attributi della classe
     void setX(int X);
     void setY(int Y);
     void setH(int H);
